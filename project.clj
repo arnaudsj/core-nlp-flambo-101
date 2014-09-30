eed70f941d1813502cd0f3f43b5b1b97db1ba3c0
Sébastien Arnaud <arnaudsj@gmail.com>
2014-09-29 21:19:01 -0400

    Updates to get job to send to spark cluster

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [yieldbot/flambo "0.3.3"]
-                 [org.apache.spark/spark-core_2.10 "1.0.2"]
-                 ; [org.apache.spark/spark-streaming_2.10 "1.0.1"]
-                 ; [org.apache.spark/spark-streaming-kafka_2.10 "1.0.1"]
-                 ; [org.apache.spark/spark-sql_2.10 "1.0.1"]
+                 [yieldbot/flambo "0.4.0-SNAPSHOT"]
+                 [org.apache.spark/spark-core_2.10 "1.1.0"]
+                 [org.apache.spark/spark-streaming_2.10 "1.1.0"]
+                 [org.apache.spark/spark-streaming-twitter_2.10 "1.1.0"]
+                 [org.apache.spark/spark-streaming-kafka_2.10 "1.1.0"]
                  [clj-time "0.8.0"]
                  [org.clojure/tools.trace "0.7.8"]
                  [clj-glob "1.0.0"]
-                 ]
+                 [me.arnaudsj/core.nlp "0.1.0"]]
   :main core-nlp-flambo-101.core
   :profiles {:dev
                 {:aot [core-nlp-flambo-101.core]}
--- src/core_nlp_flambo_101/core.clj
+++ src/core_nlp_flambo_101/core.clj
@@ -1,5 +1,6 @@
 (ns core-nlp-flambo-101.core
   (:require  [flambo.api :as f]
+             [flambo.streaming :as fs]
              [flambo.conf :as fconf]
              [clj-time.format :as tf]
              [clj-time.core :as tc]
@@ -7,14 +8,32 @@
              [clojure.java.shell :refer [sh]]
              [clojure.pprint :refer [pprint]]
              [org.satta.glob :refer [glob]])
+  (:import   [org.apache.spark.streaming.twitter TwitterUtils]
+             [org.apache.log4j Level Logger])
   (:gen-class))
 
 (def master "local[*]")
+
 (def conf {})
+
 (def env {
-          "spark.executor.memory" "4G",
+          "spark.executor.memory" "1G",
           "spark.files.overwrite" "true"
           })
+
+;; We don't need to see everything ;
+(.setLevel (Logger/getRootLogger) Level/WARN)
+
+(def ^:dynamic *app-consumer-key* "2BgYuLbLSmN43UckjIAtQT232")
+(def ^:dynamic *app-consumer-secret* "PN2bVs5LjgIHDDjiMz9NWslUTNwFqiZEr3SS88zPLLRHfrQtGg")
+(def ^:dynamic *user-screen-name* "arnaudsj")
+(def ^:dynamic *user-access-token* "13253662-UfOTvrBCMb9GokXUhhn0p5P7ctxLYzBU9EEu3zbbk")
+(def ^:dynamic *user-access-token-secret* "lT4o5CY4tseOPwDQvbY6LSiZ7PRuIja7ulJW32rme9RCk")
+
+(System/setProperty "twitter4j.oauth.consumerKey", *app-consumer-key*)
+(System/setProperty "twitter4j.oauth.consumerSecret", *app-consumer-secret*)
+(System/setProperty "twitter4j.oauth.accessToken", *user-access-token*)
+(System/setProperty "twitter4j.oauth.accessTokenSecret", *user-access-token-secret*)
 
 (defn new-spark-context []
   (let [c (-> (fconf/spark-conf)
@@ -23,14 +42,50 @@
               (fconf/set "spark.akka.timeout" "300")
               (fconf/set conf)
               (fconf/set-executor-env env))]
-    (f/spark-context c) ))
+    (fs/streaming-context c 2000) ))
 
-(defonce sc (delay (new-spark-context)))
+(defonce sc (new-spark-context))
+
+(fs/checkpoint sc "/tmp/")
+
+(defonce tweet-stream (TwitterUtils/createStream sc))
+
+;; ==================
+;; Helper functions
+;; ==================
+
+(defn extract-hashtag [s]
+  (re-find (re-pattern "#\\w+\\b") s))
+
+;; ==================
+;; Stream processing
+;; ==================
+
+;; val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+ (defonce hashTags (fs/flat-map tweet-stream (fn[t] (filter #(< 0 (count %)) [(extract-hashtag (.getText t))]))))
+
+; val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+;                      .map{case (topic, count) => (count, topic)}
+;                      .transform(_.sortByKey(false))
+
+; (defonce topCounts30 (fs/reduce-by-key-and-window hashTags (f/fn [[_ + _]]) 30000 30000))
+
+;; =============
+;; Stream output
+;; =============
+(defn -main []
+  (fs/print hashTags)
+  (.start sc)
+  (.awaitTermination sc))
 
 
+; (fs/foreach-rdd tweets
+  ; (fn[rdd t] fs/map rdd (fn[t] {:status (.getText t)}))
+  ; (-> tweets
+      ; (f/map (f/fn[[r d]] 1))
+;;      (fn[rdd t] (fs/map rdd (fn[t] {:status (.getText t)})))
+  ; ))
 
-
-;; REPL functions
-
-(defn hello-world []
-  (print "Hello, world!"))
+;; See the following for reference on the options available for rdd returned
+;; https://github.com/yusuke/twitter4j/blob/master/twitter4j-core/src/main/java/twitter4j/Status.java
+; (fs/print (fs/map tweets (fn[t] {:status (.getText t)})))
